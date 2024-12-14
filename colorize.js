c9372d8a05682080d1d1ad15c9f1392a71452371
igorrr <id.nikto@gmail.com>
2024-12-14 16:57:54 +0200

    add mini

--- colorize.js
+++ colorize.js
@@ -1,9 +1,29 @@
-javascript: (() => {
-    document.querySelectorAll('*').forEach(el => {
-        setTimeout(function () {
-            el.style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-            el.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
-            setTimeout(arguments.callee, Math.random() < 0.2 ? Math.floor(Math.random() * 9000) + 1000 : Math.floor(Math.random() * 999) + 1);
-        }, Math.random() < 0.2 ? Math.floor(Math.random() * 9000) + 1000 : Math.floor(Math.random() * 999) + 1);
-    });
+javascript:(function(){
+    var elements = document.querySelectorAll('*');
+    
+    function getRandomColor() {
+        return '#' + Math.floor(Math.random() * 16777215).toString(16);
+    }
+
+    function getRandomDelay() {
+        return Math.random() < 0.2
+            ? Math.floor(Math.random() * 9000) + 1000
+            : Math.floor(Math.random() * 999) + 1;
+    }
+
+    function changeColors(el) {
+        el.style.backgroundColor = getRandomColor();
+        el.style.color = getRandomColor();
+        setTimeout(function() {
+            changeColors(el);
+        }, getRandomDelay());
+    }
+
+    for (var i = 0; i < elements.length; i++) {
+        (function(el) {
+            setTimeout(function() {
+                changeColors(el);
+            }, getRandomDelay());
+        })(elements[i]);
+    }
 })();
